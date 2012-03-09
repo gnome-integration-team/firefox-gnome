d5670c4b78d0e4661f450a7b87553c948658773e
Reda Lazri <the.red.shortcut@gmail.com>
2012-03-09 05:46:16 +0100

    First import

--- test-theme.sh
+++ test-theme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PROFILE=default
+PROFILE=Themes
 
 source ./make-xpi.sh
 cp adwaita-firefox.xpi ~/.mozilla/firefox/*.${PROFILE}/extensions/{451500c0-902c-11e0-91e4-0800200c9a66}.xpi
 
-killall firefox firefox-bin &>/dev/null; firefox -chromebug &
+killall firefox firefox-bin &>/dev/null; firefox -P Themes -chromebug &
