03f6cf0c72f44537b64d17b80c97117016ce5aaf
Reda Lazri <the.red.shortcut@gmail.com>
2012-04-09 18:17:38 +0100

    Fixing the Toolbar

--- test-theme.sh
+++ test-theme.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PROFILE=Default
+PROFILE=Beta
 
 source ./make-xpi.sh
 cp adwaita-firefox.xpi ~/.mozilla/firefox/*.${PROFILE}/extensions/{451500c0-902c-11e0-91e4-0800200c9a66}.xpi
 
-killall firefox firefox-bin &>/dev/null; firefox -P Default -chromebug &
+killall firefox firefox-bin &>/dev/null; firefox-nightly &
