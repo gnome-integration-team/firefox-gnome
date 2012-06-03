6462e1efef49c7571178bdecaa7a135a7435bfb5
Garrett LeSage <garrett@lesage.us>
2012-06-03 16:32:32 +0200

    resetting theme testing to stable firefox

--- test-theme.sh
+++ test-theme.sh
@@ -5,4 +5,4 @@ PROFILE=default
 source ./make-xpi.sh
 cp adwaita-firefox.xpi ~/.mozilla/firefox/*.${PROFILE}/extensions/{451500c0-902c-11e0-91e4-0800200c9a66}.xpi
 
-killall firefox firefox-bin &>/dev/null; firefox-nightly &
+killall firefox firefox-bin &>/dev/null; firefox -chromebug &
