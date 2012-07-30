4a6b5d80a4cdf5b31fb9d6dbc72f236ee6c2aa25
Garrett LeSage <garrett@lesage.us>
2012-07-30 16:35:08 +0200

    changed reload and spacing of forward/back

--- test-theme.sh
+++ test-theme.sh
@@ -5,8 +5,4 @@ PROFILE=default
 source ./make-xpi.sh
 cp adwaita-firefox.xpi ~/.mozilla/firefox/*.${PROFILE}/extensions/{451500c0-902c-11e0-91e4-0800200c9a66}.xpi
 
-<<<<<<< HEAD
-killall firefox firefox-bin &>/dev/null; firefox -chromebug &
-=======
 killall firefox firefox-bin &>/dev/null; firefox &
->>>>>>> master
