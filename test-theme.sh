620001e76e214737c8108dfa5115bc1a407908e1
Bruno Ezvan <bruno@ezvan.fr>
2012-10-02 11:19:34 +0200

    Make it works with more than one *.default directory

--- test-theme.sh
+++ test-theme.sh
@@ -3,6 +3,9 @@
 PROFILE=default
 
 source ./make-xpi.sh
-cp adwaita-firefox.xpi ~/.mozilla/firefox/*.${PROFILE}/extensions/{451500c0-902c-11e0-91e4-0800200c9a66}.xpi
+for directory in ~/.mozilla/firefox/*.${PROFILE}
+do
+    cp adwaita-firefox.xpi $directory/extensions/{451500c0-902c-11e0-91e4-0800200c9a66}.xpi
+done
 
 killall firefox firefox-bin &>/dev/null; firefox &
