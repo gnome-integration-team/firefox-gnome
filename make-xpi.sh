f331538928529912cd2e06256c51b15007aa5b49
Garrett LeSage <garrett@lesage.us>
2013-01-15 13:09:23 +0100

    auto-version XPI based on RDF (and add a symlink)

--- make-xpi.sh
+++ make-xpi.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 cd theme
-zip -FS -r ../gnome-firefox.xpi *
+
+VERSION=$(cat install.rdf | grep em:version | cut -f2 -d'>' | cut -f1 -d'<')
+
+zip -FS -r ../gnome-firefox-$VERSION.xpi *
+
 cd -
+
+ln -sf gnome-firefox-$VERSION.xpi gnome-firefox.xpi
