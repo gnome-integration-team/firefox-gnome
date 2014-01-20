3bacc131c490983e39ab4f91da77145fa8c394b4
Alexander Seleznev <seleznev.ru@gmail.com>
2014-01-20 23:58:11 +0700

    Move all logic to include/Tweaks.js

--- extension/bootstrap.js
+++ extension/bootstrap.js
@@ -8,171 +8,136 @@ const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 
 Cu.import("resource://gre/modules/Services.jsm");
 
-function Tweak(options) {
-    // key, style=key, defValue=false
-    
-    this.key = options.key;
-    this.style = (options.style === undefined ? this.key : options.style);
-    this.defValue = Boolean(options.defValue);
-    
-    this.isEnabled = function(prefs) {
-        if (prefs && prefs.getPrefType(this.key)) {
-            return Boolean(prefs.getBoolPref(this.key) ^ this.defValue);
-        }
-        return this.defValue;
-    }
-}
+function include(path) {
+    var uri = Services.io.newURI(path, null, Services.io.newURI(__SCRIPT_URI_SPEC__, null, null));
+    Services.scriptloader.loadSubScript(uri.spec, this);
+};
+
+include("include/Tweaks.js");
 
 var GNOMEThemeTweak = {
-    tweaks: [
-        new Tweak({key: "newtab-page"}),
-        new Tweak({key: "relief-buttons", style: null}),
-        new Tweak({key: "tabs-border"}),
-        new Tweak({key: "urlbar-history-dropmarker"}),
-        new Tweak({key: "forward-button"}),
-        new Tweak({key: "inactive-state"}),
-    ],
-    
-    appliedStyles: [],
-    
     prefs: null,
-    
-    getTweakByKey: function(key) {
-        for (var i = 0; i < this.tweaks.length; i++) {
-            if (this.tweaks[i].key == key) {
-                return this.tweaks[i];
-            }
-        }
-        return null;
-    },
-    
+
     loadStyle: function(name) {
         let sss = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
         let uri = Services.io.newURI("chrome://gnome-theme-tweak/content/tweaks/"+name+".css", null, null);
         if (!sss.sheetRegistered(uri, sss.USER_SHEET))
             sss.loadAndRegisterSheet(uri, sss.USER_SHEET);
     },
-    
+
     unloadStyle: function(name) {
         let sss = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
         let uri = Services.io.newURI("chrome://gnome-theme-tweak/content/tweaks/"+name+".css", null, null);
         if (sss.sheetRegistered(uri, sss.USER_SHEET))
             sss.unregisterSheet(uri, sss.USER_SHEET);
     },
-    
-    setAttributes: function(window) {
-        if (!window)
-            return;
-        
-        var navbar = window.document.getElementById("nav-bar");
-        var relief_buttons = this.prefs.getPrefType("relief-buttons") && this.prefs.getBoolPref("relief-buttons");
-        if (navbar)
-            navbar.setAttribute("reliefbuttons", relief_buttons);
+
+    addListener: function(type, listener) {
+        if (typeof this._listeners[type] == "undefined") {
+            this._listeners[type] = [];
+        }
+        this._listeners[type].push(listener);
     },
-    
-    removeAttributes: function(window) {
-        if (!window)
-            return;
-        
-        var navbar = window.document.getElementById("nav-bar");
-        if (navbar)
-            navbar.removeAttribute("reliefbuttons");
+
+    removeListener: function(type, listener) {
+        if (typeof this._listeners[type] != "undefined") {
+            var listeners = this._listeners[type];
+            for (var i=0; i < listeners.length; i++) {
+                if (listeners[i] === listener) {
+                    listeners.splice(listener, 1);
+                }
+            }
+        }
     },
-    
-    windowListener: {
+
+    launchIntoExistingWindows: function(targetFunction) {
+        this._launchIntoExistingWindows(targetFunction);
+    },
+
+    _listeners: {},
+
+    _launchIntoExistingWindows: function(targetFunction) {
+        let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+        let enumerator = wm.getEnumerator("navigator:browser");
+        while (enumerator.hasMoreElements()) {
+            let window = enumerator.getNext().QueryInterface(Ci.nsIDOMWindow);
+            targetFunction(window);
+        }
+        return wm;
+    },
+
+    _onLoadWindow: function(window) {
+        if (typeof GNOMEThemeTweak._listeners["loadWindow"] != "undefined") {
+            var listeners = GNOMEThemeTweak._listeners["loadWindow"];
+            for (var i=0; i < listeners.length; i++) {
+                listeners[i](window);
+            }
+        }
+    },
+
+    _windowListener: {
         onOpenWindow: function(aWindow) {
             let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
             domWindow.addEventListener("load", function onLoad() {
                 domWindow.removeEventListener("load", onLoad, false);
-                GNOMEThemeTweak.setAttributes(domWindow);
+                GNOMEThemeTweak._onLoadWindow(domWindow);
             }, false);
         },
-        
-        onCloseWindow: function(aWindow) { },
-        onWindowTitleChange: function(aWindow, aTitle) { }
+
+        onCloseWindow: function(aWindow) {},
+        onWindowTitleChange: function(aWindow, aTitle) {}
     },
-    
+
+    observe: function(subject, topic, data) {
+        if (topic != "nsPref:changed")
+            return;
+
+        for (var i in Tweaks) {
+            if (Tweaks[i].key == data) {
+                this.log(data);
+                Tweaks[i].disable();
+                Tweaks[i].enable();
+                break;
+            }
+        }
+    },
+
+    log: function(message) {
+        var console = Cc["@mozilla.org/consoleservice;1"]
+                        .getService(Ci.nsIConsoleService);
+        console.logStringMessage(message);
+    },
+
     init: function() {
         this.prefs = Cc["@mozilla.org/preferences-service;1"]
                        .getService(Components.interfaces.nsIPrefService)
                        .getBranch("extensions.gnome-theme-tweak.");
-        
+
         // Removing older keys...
         if (this.prefs.getPrefType("restore-button"))
             this.prefs.clearUserPref("restore-button");
-        
-        this.prefs.addObserver("", this, false);
-        
-        for (var i = 0; i < this.tweaks.length; i++) {
-            var tweak = this.tweaks[i];
-            
-            if (tweak.style && tweak.isEnabled(this.prefs)) {
-                this.loadStyle(tweak.style);
-                this.appliedStyles.push(tweak.style);
-            }
-        }
-        
-        let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
 
-        // Load into any existing windows
-        let enumerator = wm.getEnumerator("navigator:browser");
-        while (enumerator.hasMoreElements()) {
-            let window = enumerator.getNext().QueryInterface(Ci.nsIDOMWindow);
-            this.setAttributes(window);
-        }
+        this.prefs.addObserver("", this, false);
+
+        let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
 
         // Load into any new windows
-        wm.addListener(this.windowListener);
+        wm.addListener(this._windowListener);
+
+        for (var i in Tweaks) {
+            Tweaks[i].enable();
+        }
     },
-    
+
     uninit: function() {
-        this.prefs.removeObserver("", this);
-        
-        for (var i = 0; i < this.appliedStyles.length; i++) {
-            this.unloadStyle(this.appliedStyles[i]);
+        for (var i in Tweaks) {
+            Tweaks[i].disable();
         }
-        
+
         let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
-        
-        // Stop watching for new windows
-        wm.removeListener(this.windowListener);
-        
-        // Unload from any existing windows
-        let enumerator = wm.getEnumerator("navigator:browser");
-        while (enumerator.hasMoreElements()) {
-            let window = enumerator.getNext().QueryInterface(Ci.nsIDOMWindow);
-            this.removeAttributes(window);
-        }
-    },
-    
-    observe: function(subject, topic, data) {
-        if (topic != "nsPref:changed")
-            return;
-        
-        var tweak = this.getTweakByKey(data);
-        if (tweak) {
-            if (tweak.key == "relief-buttons") {
-                let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
-                
-                // Load into any existing windows
-                let enumerator = wm.getEnumerator("navigator:browser");
-                while (enumerator.hasMoreElements()) {
-                    let window = enumerator.getNext().QueryInterface(Ci.nsIDOMWindow);
-                    this.setAttributes(window);
-                }
-            }
-            
-            if (tweak.style) {
-                if (tweak.isEnabled(this.prefs)) {
-                    this.loadStyle(tweak.style);
-                    this.appliedStyles.push(tweak.style);
-                }
-                else {
-                    this.unloadStyle(tweak.style);
-                    this.appliedStyles.splice(this.appliedStyles.indexOf(tweak.style), 1);
-                }
-            }
-        }
+        wm.removeListener(this._windowListener);
+
+        this.prefs.removeObserver("", this);
     },
 }
 
@@ -183,7 +148,7 @@ function startup(data, reason) {
 function shutdown(data, reason) {
     if (reason == APP_SHUTDOWN)
         return;
-    
+
     GNOMEThemeTweak.uninit();
 }
 
---
+++ extension/include/Tweaks.js
@@ -0,0 +1,123 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+var Tweaks = {
+    /*
+    exampleTweak: {
+        key: "example-tweak",
+        enable: function() {
+            // do something...
+        },
+        disable: function() {
+            // do something...
+        },
+    },
+    */
+
+    newtabPage: {
+        key: "newtab-page",
+        enable: function() {
+            if (!this._isEnabled && GNOMEThemeTweak.prefs.getPrefType(this.key) && GNOMEThemeTweak.prefs.getBoolPref(this.key)) {
+                GNOMEThemeTweak.loadStyle(this.key);
+                this._isEnabled = true;
+            }
+        },
+        disable: function() {
+            this._isEnabled && GNOMEThemeTweak.unloadStyle(this.key); this._isEnabled = false;
+        },
+        _isEnabled: false
+    },
+
+    tabsBorder: {
+        key: "tabs-border",
+        enable: function() {
+            if (!this._isEnabled && GNOMEThemeTweak.prefs.getPrefType(this.key) && GNOMEThemeTweak.prefs.getBoolPref(this.key)) {
+                GNOMEThemeTweak.loadStyle(this.key);
+                this._isEnabled = true;
+            }
+        },
+        disable: function() {
+            this._isEnabled && GNOMEThemeTweak.unloadStyle(this.key); this._isEnabled = false;
+        },
+        _isEnabled: false
+    },
+
+    urlbarHistoryDropmarker: {
+        key: "urlbar-history-dropmarker",
+        enable: function() {
+            if (!this._isEnabled && GNOMEThemeTweak.prefs.getPrefType(this.key) && GNOMEThemeTweak.prefs.getBoolPref(this.key)) {
+                GNOMEThemeTweak.loadStyle(this.key);
+                this._isEnabled = true;
+            }
+        },
+        disable: function() {
+            this._isEnabled && GNOMEThemeTweak.unloadStyle(this.key); this._isEnabled = false;
+        },
+        _isEnabled: false
+    },
+
+    forwardButton: {
+        key: "forward-button",
+        enable: function() {
+            if (!this._isEnabled && GNOMEThemeTweak.prefs.getPrefType(this.key) && GNOMEThemeTweak.prefs.getBoolPref(this.key)) {
+                GNOMEThemeTweak.loadStyle(this.key);
+                this._isEnabled = true;
+            }
+        },
+        disable: function() {
+            this._isEnabled && GNOMEThemeTweak.unloadStyle(this.key); this._isEnabled = false;
+        },
+        _isEnabled: false
+    },
+
+    inactiveState: {
+        key: "inactive-state",
+        enable: function() {
+            if (!this._isEnabled && GNOMEThemeTweak.prefs.getPrefType(this.key) && GNOMEThemeTweak.prefs.getBoolPref(this.key)) {
+                GNOMEThemeTweak.loadStyle(this.key);
+                this._isEnabled = true;
+            }
+        },
+        disable: function() {
+            this._isEnabled && GNOMEThemeTweak.unloadStyle(this.key); this._isEnabled = false;
+        },
+        _isEnabled: false
+    },
+
+    reliefButtons: {
+        key: "relief-buttons",
+
+        enable: function() {
+            if (!this._isEnabled && GNOMEThemeTweak.prefs.getPrefType(this.key) && GNOMEThemeTweak.prefs.getBoolPref(this.key)) {
+                GNOMEThemeTweak.addListener("loadWindow", this._setAttributes);
+                GNOMEThemeTweak.launchIntoExistingWindows(this._setAttributes);
+                this._isEnabled = true;
+            }
+        },
+
+        disable: function() {
+            if (this._isEnabled) {
+                GNOMEThemeTweak.removeListener("loadWindow", this._setAttributes);
+                GNOMEThemeTweak.launchIntoExistingWindows(this._removeAttributes);
+                this._isEnabled = false;
+            }
+        },
+
+        _isEnabled: false,
+
+        _setAttributes: function(window) {
+            if (!window) return;
+            var navbar = window.document.getElementById("nav-bar");
+            navbar && navbar.setAttribute("reliefbuttons", true);
+        },
+
+        _removeAttributes: function(window) {
+            if (!window) return;
+            var navbar = window.document.getElementById("nav-bar");
+            navbar && navbar.removeAttribute("reliefbuttons");
+        },
+    },
+}
