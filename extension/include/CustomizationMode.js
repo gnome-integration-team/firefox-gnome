0c0b0a891bb9374959296eb56d79d3c2492d8a1c
Alexander Seleznev <seleznev.ru@gmail.com>
2014-04-18 23:29:17 +0700

    Make Tweaks.js more simple
    Changes:
    
    1. New tweak's types: stylesheet, attributes.
    
    2. Use new types for existing tweaks.
    
    3. CustomizationMode.js (code for adding link).
    
    4. Remove sizeModeChange event listeners.
    
    5. Small code fixes.

---
+++ extension/include/CustomizationMode.js
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var CustomizationMode = {
+    _addLink: function(window) {
+        if (!window) return;
+
+        var element = window.document.getElementById("customization-toolbar-visibility-button");
+        if (!element) return;
+
+        var open_prefs = "BrowserOpenAddonsMgr('addons://detail/' + encodeURIComponent('{05f1cdaa-7474-43fa-9f69-c697555f4ea8}') + '/preferences');"
+
+        var link = window.document.createElement("label");
+        link.setAttribute("id", "customization-gnome-tweaks-link");
+        link.setAttribute("class", "text-link customizationmode-link");
+        link.setAttribute("value", "GNOME Tweaks"); // Localization?
+        link.setAttribute("onclick", open_prefs);
+
+        element.parentNode.insertBefore(link, element.nextSibling);
+    },
+
+    _removeLink: function(window) {
+        if (!window) return;
+        var button = window.document.getElementById("customization-gnome-tweaks-link");
+        button && button.remove();
+    }
+}
